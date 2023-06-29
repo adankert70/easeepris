5d8ce49f7a06425828c932462ad8ca24f924c03f
Asbjørn Lenovo <adankert@gmail.com>
2023-06-29 20:50:18 +0200

    Code cleanup

--- forbruk.py
+++ forbruk.py
@@ -18,44 +18,35 @@ def main():
     #print(f"Måned: {last_month}")
     #print(f"Siste dag: {last_day}")
     #print(f"År: {year}")
-    print("Start: ", first_day_previous_month)
-    print("Slutt: ", last_day_previous_month)
+    print("Rapport start: ", first_day_previous_month)
+    print("Rapport slutt: ", last_day_previous_month)
     priser = hentpriser(year,last_month,last_day)
     user = os.getenv('API_USER')
     pwd = os.environ.get('API_PASSWORD')
     token = autentiser(user,pwd)
     if not token:
-        print("Jeg gir opp...")
+        print("Jeg gir opp, logon feilet...")
         sys.exit()
-    #print (f"Fikk token! {token}")
-    print("Logged on!")
+    print("Logged on to Easee API!")
     ladere = chargers(token)
-    if not chargers:
+    if not ladere:
         sys.exit()
-    fogp = {}
     for id in ladere.keys():
         idpris = 0
-        #print(f"id: {id}\tNavn: {ladere[id]}")
         fjson = forbruk(token,id,first_day_previous_month,last_day_previous_month)
-        ## Parse JSON, find the consumptions by Hour
         creport = []
         for cjson in fjson:
-            #print(cjson)
             consumption = cjson['consumption']
             if consumption == 0:
                 continue
             dato = cjson['date']
             factor = priser[dato]
-            #print(f"Factor: {factor}")
-            pris = consumption * factor # Pris må finnes...
-            #print(f"Pris for {dato}:forbruk {consumption} {pris}")
+            pris = consumption * factor 
             idpris += pris
             creport.append({"date": dato, "consumption": consumption, "price": pris})
-        fogp[id] = creport
         idpris = round(idpris,2)
         print(f"Pris for lader {id} {ladere[id]}\t{idpris}")
         totalpris += idpris
-    #print(fogp)
     totalpris = round(totalpris,2)
     print(f"Totalpris for perioden:\t{totalpris}")
 
